1a6c1ff63d361814ba6c78a2deabf3fc45ec60be
Silver-Core <moorcroftmanor@yandex.com>
2017-10-28 17:29:16 +0300

    +Helper Functions +EmbGen

--- BotClient.py
+++ BotClient.py
@@ -35,7 +35,30 @@ async def on_ready():
 
 
 ########################################################################################################################
-#                                                    UTILITY//FUN                                                      #
+#                                                 HELPER FUNCTIONS                                                     #
+########################################################################################################################
+
+def EmbGen(title=None, description=None, footer=None, author=None, *fields, color=0xff8040):
+    """This Function will be used to generate embeds on the go, For any command to use if needed."""
+    url = r"https://cdn.discordapp.com/app-icons/369099294579359744/85818996c0ccfd1030b096f4c3dcf23f.png"
+    embed=discord.Embed(title=title, description=description, color=color)
+
+    embed.set_author(
+        name=author,
+        icon_url=url
+    )
+
+    if fields is not None:
+        for name, value in fields:
+            embed.add_field(name=name, value=value, inline=True)
+
+    if footer is not None:
+        embed.set_footer(text=footer)
+
+    return embed
+    
+########################################################################################################################
+#                                                   COMMANDS                                                           #
 ########################################################################################################################
 
 
