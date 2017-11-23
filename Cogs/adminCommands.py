7478928d8cd0a78a12ee463930e1d749ba8b17c7
Silver-Core <moorcroftmanor@yandex.com>
2017-11-23 23:40:34 +0300

    Rewritten

--- Cogs/adminCommands.py
+++ Cogs/adminCommands.py
@@ -1,7 +1,9 @@
 """ Contains admin only commands, c#clear, c#ping .etc... """
+import discord
 import discord.ext.commands as commands
 from HelperFunctions.EmbedGenerator import EmbGen
 from time import sleep
+import datetime
 
 
 class AdminCommands:
@@ -9,78 +11,21 @@ class AdminCommands:
         self.bot = bot
 
     @commands.has_permissions()
-    @commands.command(pass_context=True, hidden=True)
-    async def clear(self, ctx, number):
+    @commands.command(hidden=True)
+    async def clear(self, ctx, number: int):
         """ Clears chat log 'n' messages."""
-        mgs = []
-        number = int(number)  # Converting the amount of messages to delete to an integer
-        async for x in self.bot.logs_from(ctx.message.channel, limit=number):
-            mgs.append(x)
-        await self.bot.delete_messages(mgs)
+        await ctx.channel.purge(limit=number+1)
 
     @commands.has_permissions()
-    @commands.command(pass_context=True, hidden=True)
-    async def crash(self, ctx):
-        """ Induces a crash [Only usable by NN/SC & gum]. """
-        if ctx.message.author.id == "205633407093309440" or ctx.message.author.id == "281120477945266177":
-            await self.bot.say("Goodbye Cruel World...")
-            await self.bot.close()
-        else:
-            await self.bot.say("You do not have permissions to make me crash.")
-
-    @commands.has_permissions()
-    @commands.command(pass_context=True, hidden=True)
+    @commands.command(hidden=True)
     async def ping(self, ctx):
         """ Tests self.bot Functionality """
         print("Pinged by..." + ctx.message.author.name)
-        msg = ctx.message
-        await self.bot.add_reaction(msg, u"\U0001F9C0")
-        await self.bot.say(embed=EmbGen(title="Pong!", description=":ping_pong:!"))
-        sleep(1)
-    
-    @commands.has_permissions()
-    @commands.command(pass_context=True)
-    async def debug(self, ctx, *, code: str):
-        """Evaluates code."""
-        import inspect
-        import sys
-        import StringIO
-        stdout = sys.stdout
-        s = StringIO.StringIO()
-        sys.stdout = s
-        code = code.strip('` ')
-        result = None
-
-        env = {
-            'bot': self.bot,
-            'ctx': ctx,
-            'message': ctx.message,
-            'server': ctx.message.server,
-            'channel': ctx.message.channel,
-            'author': ctx.message.author
-        }
-
-        env.update(globals())
-
-        try:
-            result = eval(code, env)
-            sys.stdout = stdout
-            s.seek(0)
-            res = s.read()
-            if inspect.isawaitable(result):
-                result = await result
-        except Exception as e:
-            await self.bot.say(embed=EmbGen(title="Debugging", description=type(e).__name__ + ': ' + str(e)))
-            return
-        await self.bot.say(embed=EmbGen(title="Debugging...", description=res, footer="Python 3.6.2"))
-            
-    @commands.has_permissions()
-    @commands.command(pass_context=True, hidden=True)
-    async def ver(self, ctx):
-        """ Print Version Control and Console """
-        await self.bot.add_reaction(ctx.message, u"\U0001F9C0")
-        await self.bot.say(embed=EmbGen(title="Hello World!",
-                                        description="***Caseus v(This Is Broken) reporting for duty!***".format()))
+        now = datetime.datetime.utcnow()
+        delta = now - ctx.message.created_at
+        re = f'Responded in {delta.microseconds} microseconds.'
+        e = EmbGen(title='Pong!', description=':ping_pong:!', footer=re)
+        await ctx.send(embed=e)
 
 
 def setup(bot):
