25cc915d6adec8bef7013995db70557e390c071b
Silver-Core <moorcroftmanor@yandex.com>
2017-11-25 00:23:43 +0300

    Update adminCommands.py

--- Cogs/adminCommands.py
+++ Cogs/adminCommands.py
@@ -20,43 +20,6 @@ class AdminCommands:
 
     @commands.has_permissions()
     @commands.command(hidden=True)
-    async def debug(self, ctx, *, code: str):
-        """Evaluates code."""
-        
-        code = code.strip('` ')
-        python = '```py\n{}\n```'
-        result = None
-        env = {
-            'bot': self.bot,
-            'ctx': ctx,
-            'message': ctx.message,
-            'server': ctx.message.guild,
-            'channel': ctx.message.channel,
-            'author': ctx.message.author
-        }
-        
-        env.update(globals())
-
-        try:
-            result = eval(code, env)
-            if inspect.isawaitable(result):
-                result = await result
-        except Exception as e:
-            await ctx.send(embed=Embed(
-                title='Exception Raised:',
-                description=python.format(type(e).__name__+ ": " + str(e)),
-                color=discord.Color.dark_red()
-            ))
-            return
-
-        await ctx.send(embed=Embed(
-            title="stdout",
-            description=python.format(result),
-            color=discord.Color.green()
-        ))
-
-    @commands.has_permissions()
-    @commands.command(hidden=True)
     async def ping(self, ctx):
         """ Tests self.bot Functionality """
         print("Pinged by..." + ctx.message.author.name)
@@ -65,7 +28,6 @@ class AdminCommands:
         re = f'Responded in {delta.microseconds} microseconds.'
         e = EmbGen(title='Pong!', description=':ping_pong:!', footer=re)
         await ctx.send(embed=e)
-
 
 def setup(bot):
     bot.add_cog(AdminCommands(bot))
