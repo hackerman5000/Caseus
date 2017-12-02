a6135af65b39a3209c90e5bc3a9e0f3571c286c1
Silver-Core <moorcroftmanor@yandex.com>
2017-12-02 16:05:05 +0300

    Update adminCommands.py

--- Cogs/adminCommands.py
+++ Cogs/adminCommands.py
@@ -92,7 +92,7 @@ class AdminCommands:
         """Gets a User's/Member's profile."""
         wine = main(usr.id)
         last_message = await ctx.channel.history().get(author__name=f'{usr.name}')
-        joined_at = strftime('%B %d %Y at %I:%M %p', usr.joined_at)
+        joined_at = usr.joined_at.strftime('%B %d %Y at %I:%M %p')
         profile = Embed(title=f"{usr.name}'s Profile",
                         color=usr.color)
         profile.set_thumbnail(url=usr.avatar_url)
