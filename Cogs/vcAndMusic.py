2c48848e243b0016430e8679efc9b9caec48434c
Silver-Core <moorcroftmanor@yandex.com>
2017-12-25 18:58:26 +0300

    Update vcAndMusic.py

--- Cogs/vcAndMusic.py
+++ Cogs/vcAndMusic.py
@@ -3,6 +3,7 @@ import asyncio
 import discord
 import discord.ext.commands as commands
 from discord import Embed
+from discord import opus
 
 
 class VCAndMusic:
@@ -12,6 +13,16 @@ class VCAndMusic:
     @commands.command()
     async def connect(self, ctx):
         """Connects to a Voice Channel."""
+
+        if discord.opus.is_loaded():
+            print("Opus is loaded...")
+        else:
+            for opus_lib in ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']:
+                try:
+                    discord.opus.load_opus(opus_lib)
+                except OSError:
+                    pass
+
         if ctx.message.author.voice is None:
             await ctx.send(embed=Embed(title=":(",
                                        description="Please Join a Voice Channel before using this command...",
