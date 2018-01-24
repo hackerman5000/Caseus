3a950247cb3ea6b04138cf2dedc9bcc371be4a75
Silver-Core <moorcroftmanor@yandex.com>
2018-01-24 17:57:02 +0300

    Who needs SQL when you have Python Dictionaries?

--- WineRecords.py
+++ WineRecords.py
@@ -1,23 +1,8 @@
-def main(UID):
-    import sqlite3 as sql
-
-    db = sql.connect(r"WineRecords.db")
-    cur = db.cursor()
-
-    db.execute("CREATE TABLE IF NOT EXISTS USERS(ID TEXT, NUM INTEGER)")
-    cur.execute("SELECT * FROM USERS WHERE ID = {}".format(UID))
-    Query = cur.fetchall()
-
-    if len(Query) > 0:
-        print("Updating Values in DB for {}".format(UID))
-        db.execute("UPDATE USERS SET NUM = NUM + 1 WHERE ID = {};".format(UID))
-        db.commit()
-
-    else:
-        print("Record for {} not found, Creating one instead...".format(UID))
-        db.execute("INSERT INTO USERS VALUES ({}, 0)".format(UID))
-        db.commit()
-
-    cur.execute("SELECT * FROM USERS WHERE ID = {}".format(UID))
-    vals = cur.fetchall()[0]
-    return "*<@{0}> has been given {1} glasses of :wine_glass:!*".format(vals[0], (vals[1]+1))
+WineRecords = {
+}
+def main(ctx):
+    try:
+        WineRecords[str(ctx.author.id)] += 1
+    except KeyError:
+        WineRecords[str(ctx.author.id)] = 1
+    return f"*{ctx.author.mention} has been given {WineRecords[str(ctx.author.id)]} glasses of :wine_glass:!*"
