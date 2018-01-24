00d77a58764995a57fbba65ab79961c535f01011
Silver-Core <moorcroftmanor@yandex.com>
2018-01-24 18:24:56 +0300

    Update WineRecords.py

--- WineRecords.py
+++ WineRecords.py
@@ -1,9 +1,3 @@
 WineRecords = {
     "ID": "Wine", 
 }
-def main(ctx):
-    try:
-        WineRecords[str(ctx.author.id)] += 1
-    except KeyError:
-        WineRecords[str(ctx.author.id)] = 1
-    return f"*{ctx.author.mention} has been given {WineRecords[str(ctx.author.id)]} glasses of :wine_glass:!*"
