7c0352a61c91ec8d97a951fc2d894521a7d728e5
Silver-Core <moorcroftmanor@yandex.com>
2018-02-08 11:51:23 +0300

    Update AgCommands.py

--- Cogs/AgCommands.py
+++ Cogs/AgCommands.py
@@ -39,63 +39,63 @@ class AgCommands:
             # Remove `foo`
             return content.strip('` \n')
 
-            #########################################
-            #              Commands                 #
-            #########################################
+        #########################################
+        #              Commands                 #
+        #########################################
+        
+        @commands.command(hidden=True)
+        @commands.is_owner()
+        async def WineDB(self, ctx):
+            '''Send a 'copy' of the DB if needed.'''
+            from WineRecords import WineRecords
+            await self.caseus.get_user(self.caseus.owner_id).send(WineRecords)
 
-            @commands.command(hidden=True)
-            @commands.is_owner()
-            async def WineDB(self, ctx):
-                '''Send a 'copy' of the DB if needed.'''
-                from WineRecords import WineRecords
-                await self.caseus.get_user(self.caseus.owner_id).send(WineRecords)
+        @commands.command(hidden=True)
+        @commands.is_owner()
+        async def debug(self, ctx, *, body: str):
+            '''Evaluates a chunk of code.'''
+            env = {
+            'bot': self.caseus,
+            'ctx': ctx,
+            }.update(globals())
 
-            @commands.command(hidden=True)
-            @commands.is_owner()
-            async def debug(self, ctx, *, body: str):
-                '''Evaluates a chunk of code.'''
-                env = {
-                    'bot': self.caseus,
-                    'ctx': ctx,
-                }.update(globals())
+            body = self.CleanupCode(body)
+            stdout = io.StringIO()
+            toCompile = f'async def func():\n{textwrap.indent(body, "  ")}'
 
-                body = self.CleanupCode(body)
-                stdout = io.StringIO()
-                toCompile = f'async def func():\n{textwrap.indent(body, "  ")}'
-
-                try:
-                    exec(toCompile, env)
-                except Exception as e:
-                    return await ctx.send(Embed(title="Exception:",
+            try:
+                exec(toCompile, env)
+            except Exception as e:
+                return await ctx.send(Embed(title="Exception:",
                                                 description=f"```py\n{e.__class__.__name__}{e}\n```",
                                                 color=discord.Color.dark_red))
-                func = env['func']
-                try:
-                    with redirect_stdout(stdout):
-                        ret = await func()
+            func = env['func']
+            try:
+                with redirect_stdout(stdout):
+                 ret = await func()
 
-                except Exception as e:
-                    value = stdout.getvalue()
-                    await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+            except Exception as e:
+                 value = stdout.getvalue()
+                 await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
 
-                else:
-                    value = stdout.getvalue()
-
-                    if ret is None:
+            else:
+                 value = stdout.getvalue()
+                 if ret is None:
                         if value:
                             await ctx.send(f'```py\n{value}\n```')
-                    else:
-                        self._last_result = ret
-                        await ctx.send(f'```py\n{value}{ret}\n```')
+          
+                        else:
+                            self._last_result = ret
+                            await ctx.send(f'```py\n{value}{ret}\n```')
 
-            @commands.command(hidden=True)
-            async def ping(self, ctx):
-                    """ Tests self.bot Functionality """
-                    VERSION = os.getenv('HEROKU_RELEASE_VERSION')
-                    e = Embed(title='Pong!', description=':ping_pong:!', color=discord.Color.green())
-                    e.set_footer(text=f"Caseus Version {VERSION}")
-                    e.add_field(name="Latency:", value=f"Responded in {round(self.bot.latency, 2)} microseconds.")
-                    await ctx.send(embed=e)
+        @commands.command(hidden=True)
+        async def ping(self, ctx):
+            """ Tests self.bot Functionality """
+            VERSION = os.getenv('HEROKU_RELEASE_VERSION')
+            e = Embed(title='Pong!', description=':ping_pong:!', color=discord.Color.green())
+            e.set_footer(text=f"Caseus Version {VERSION}")
+            e.add_field(name="Latency:", value=f"Responded in {round(self.bot.latency, 2)} microseconds.")
+            await ctx.send(embed=e)
 
 def setup(cas):
         cas.add_cog(AgCommands(cas))
